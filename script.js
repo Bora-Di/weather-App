a01dbd35394c255ba99ddb5eb7fde0d09e2361e2
Bora-Di <mbou7841@gmail.com>
2024-01-12 20:58:08 +0100

    script js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const cityInput = document.querySelector(".city-input");
-const searchButton = document.querySelector(".search-btn");
+const searchButton = document.querySelector(".search-button");
+const API_key = "485b09b08a7fe230b73c96870395c2e4";
 
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if (!cityName) return;
-    console.log(cityName);
+    const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_key}';
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+
+    })
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
