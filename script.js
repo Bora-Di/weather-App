be8df0752317c95822bd077fc88edf8b01395889
Bora-Di <mbou7841@gmail.com>
2024-01-12 18:10:34 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector(".search-btn");
+const searchButton = document.querySelector(".search-btn");
+
+const getCityCoordinates = () => {
+    const cityInput =
+}
+searchButton.addEventListener("click", getCityCoordinates);
