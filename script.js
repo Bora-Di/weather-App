89dfdabe802dde2dc213c13e426ab7e97e2a65b8
Bora-Di <mbou7841@gmail.com>
2024-01-13 01:23:40 +0100

    js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_key}';
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-        if (!data.length) return alert('No coordinates found for ${cityName}');
-        const { name, lat, lon } = data[0];
-        getWeatherDetails(name, lat, lon);
+        const uniqueForecastDays = [];
+        const fiveDaysForecast = data.list.filter(forecast => {
+            const forecastDate = new Date(forecastDate);
+        })
     }).catch(() => {
-        alert("an erroe occurence while fetching the coordinates")
+        alert("an error occurred while fetching the weather forecast")
     });
 }
 
