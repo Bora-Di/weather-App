58e1628dca8968b643a7a622f6e1254fefe49c7d
Bora-Di <mbou7841@gmail.com>
2024-01-13 01:10:50 +0100

    fetch data

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-button");
-const API_key = "485b09b08a7fe230b73c96870395c2e4";
+const API_key = "fa38c7ea4ecf776d195829e6d5850f1f";
+
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_key}';
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        if (!data.length) return alert('No coordinates found for ${cityName}');
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
+    }).catch(() => {
+        alert("an erroe occurence while fetching the coordinates")
+    });
+}
 
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
@@ -8,10 +20,12 @@ const getCityCoordinates = () => {
     const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_key}';
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        console.log(data)
+        if (!data.length) return alert('No coordinates found for ${cityName}');
+        const { name, lat, lon } = data[0];
+        getWeatherDetails(name, lat, lon);
     }).catch(() => {
-
-    })
+        alert("an erroe occurence while fetching the coordinates")
+    });
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
