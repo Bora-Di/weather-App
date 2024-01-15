56225904f4e4f8a9870fbdea97a4a4078c0492b9
Bora-Di <mbou7841@gmail.com>
2024-01-15 16:45:23 +0100

    weather-card js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-button");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardDiv = document.querySelector(".weather-cards");
+
+
 const API_key = "485b09b08a7fe230b73c96870395c2e4";
+
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) {
+        return ``;
+    }else{
+        return `
+            <li class="card">
+            <h3> (${weatherItem.dt_txt.split("")[0]})</h3>
+            <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather">
+            <h4>Temp: ${(weatherItem.main.temp - 273015).toFixed(2)} °C</h4>
+            <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+            <h4>Humidity: ${weatherItem.main.humidity} %</h4>
+            </li>`;
+    }
+    
+}
 
 const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_key}`;
@@ -8,12 +28,24 @@ const getWeatherDetails = (cityName, lat, lon) => {
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getdays();
+            const forecastDate = new Date(forecast.dt_txt).getDate();
             if(!uniqueForecastDays.includes(forecastDate))
             return uniqueForecastDays.push(forecastDate);
         })
 
-        console.log('fiveDaysForecast');
+        cityInput.value = "";
+        currentWeatherDiv.innerHTML = "";
+        weatherCardDiv.innerHTML ="";
+
+        console.log(fiveDaysForecast);
+    
+        fiveDaysForecast.forEach((weatherItem, index) => {
+            if(index === 0){
+            currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            }else{
+             weatherCardDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+            }
+        });
 
     }).catch(() => {
         alert("an error occurred while fetching the weather forecast")
