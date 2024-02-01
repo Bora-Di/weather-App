199ed236055a592a089d0e7e64cdd5416c707bec
Bora-Di <mbou7841@gmail.com>
2024-02-01 18:10:07 +0100

    commit

--- js/api.js
+++ js/api.js
@@ -10,8 +10,8 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) {
         return `
          <div class="details">
-            <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-            <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h4>
+            <h2>${cityName} | ${weatherItem.dt_txt.split(" ")[0]} </h2>
+            <h4>${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h4>
             <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
             <h4>Humidity: ${weatherItem.main.humidity} %</h4>
         </div>
@@ -23,7 +23,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     }else{
         return `
             <li class="card">
-            <h3> (${weatherItem.dt_txt.split(" ")[0]})</h3>
+            <h3> ${weatherItem.dt_txt.split(" ")[0]}</h3>
             <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather">
             <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)} °C</h4>
             <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
